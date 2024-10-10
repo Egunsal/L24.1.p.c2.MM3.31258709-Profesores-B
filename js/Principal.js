8b4fb80febcc681f535d947a42043b1866823016
Willian Alcon <1001.31258709.ucla@gmail.com>
2024-10-10 11:38:07 -0400

    Principal.js

--- js/Principal.js
+++ js/Principal.js
@@ -4,4 +4,7 @@ let contratado1 = new Cl_Contratado("Rafael",30,15)
 
 
 let salida = document.getElementById("salida")
-salida.innerHTML = "Ingreso total del profesor "+contratado1.mostrarNombre()+": "+contratado1.sueldoTotal();
+salida.innerHTML ="Nombre del profesor contratado: "+contratado1.mostrarNombre();
+salida.innerHTML +="<br>Monto del Bono: "+contratado1.mostrarBono();
+salida.innerHTML +="<br>Cantidad de horas: "+contratado1.mostrarHoras();
+salida.innerHTML += "<br>Ingreso total del profesor "+contratado1.mostrarNombre()+": "+contratado1.sueldoTotal();
